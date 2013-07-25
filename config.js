32482da46038f3e495ff6e5930642fb16ef93a67
Espen Fjellvær Olsen <espen@mrfjo.org>
2013-07-25 22:43:56 +0200

    Clean up loading of metrics object

--- config.js
+++ config.js
@@ -1,66 +1,59 @@
 var graphite = "http://graphite.mrfjo.org/render/";
 
-var loaded = 0;
-
-$(function() {
-    if (loaded === 0) {
-        metrics = [ {
-            name: "Balkong",
-            metricPath: "mrfjo.hjemme.temp.telldus.Balkong",
-            type: "temperature",
-            suffix: "°C",
-            scale: "",
-            dashboard: true,
-            single: false
-        }, {
-            name: "Soverom",
-            metricPath: "mrfjo.hjemme.temp.telldus.Soverom",
-            type: "temperature",
-            suffix: "°C",
-            dashboard: true,
-            scale: "",
-            single: ""
-        }, {
-            name: "Office desk",
-            metricPath: "mrfjo.hjemme.temp.28-000002dcb9bc",
-            type: "temperature",
-            dashboard: false,
-            suffix: "°C",
-            scale: "0.001",
-            single: ""
-        }, {
-            name: "Outside wall",
-            metricPath: "mrfjo.hjemme.temp.28-0000044ddd1d",
-            type: "temperature",
-            dashboard: false,
-            suffix: "°C",
-            scale: "0.001",
-            single: ""
-        }, {
-            name: "Barometer",
-            metricPath: "mrfjo.hjemme.pressure.bmp085_pressure",
-            type: "pressure",
-            dashboard: true,
-            suffix: "",
-            scale: "Pa",
-            single: true
-        }, {
-            name: "Light",
-            metricPath: "mrfjo.hjemme.light.tsl2561_lux",
-            type: "light",
-            dashboard: false,
-            suffix: "lx",
-            scale: "",
-            single: true
-        }, {
-            name: "Humidity",
-            metricPath: "mrfjo.hjemme.humidity.dht11_humidity",
-            type: "humidity",
-            dashboard: false,
-            suffix: "%",
-            scale: "",
-            single: true
-        } ];
-        loaded = 1;
-    }
-});
+var metrics = metrics || [ {
+    name: "Balkong",
+    metricPath: "mrfjo.hjemme.temp.telldus.Balkong",
+    type: "temperature",
+    suffix: "°C",
+    scale: "",
+    dashboard: true,
+    single: false
+}, {
+    name: "Soverom",
+    metricPath: "mrfjo.hjemme.temp.telldus.Soverom",
+    type: "temperature",
+    suffix: "°C",
+    dashboard: true,
+    scale: "",
+    single: ""
+}, {
+    name: "Office desk",
+    metricPath: "mrfjo.hjemme.temp.28-000002dcb9bc",
+    type: "temperature",
+    dashboard: false,
+    suffix: "°C",
+    scale: "0.001",
+    single: ""
+}, {
+    name: "Outside wall",
+    metricPath: "mrfjo.hjemme.temp.28-0000044ddd1d",
+    type: "temperature",
+    dashboard: false,
+    suffix: "°C",
+    scale: "0.001",
+    single: ""
+}, {
+    name: "Barometer",
+    metricPath: "mrfjo.hjemme.pressure.bmp085_pressure",
+    type: "pressure",
+    dashboard: true,
+    suffix: "",
+    scale: "Pa",
+    single: true
+}, {
+    name: "Light",
+    metricPath: "mrfjo.hjemme.light.tsl2561_lux",
+    type: "light",
+    dashboard: false,
+    suffix: "lx",
+    scale: "",
+    single: true
+}, {
+    name: "Humidity",
+    metricPath: "mrfjo.hjemme.humidity.dht11_humidity",
+    type: "humidity",
+    dashboard: false,
+    suffix: "%",
+    scale: "",
+    single: true
+} ];
